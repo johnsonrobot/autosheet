4b77d78e1aba9149dbdc2bd982b728ce2da4d53d
johnsonrobot <johnson860312@gmail.com>
2021-07-26 21:02:20 +0800

    config modify and submit

--- sheet.py
+++ sheet.py
@@ -5,25 +5,16 @@ import configparser
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-url = config.get("url", "path")
-user = config.get("user", "user")
-x = config.get("user", "x")
-id = config.get("user", "id")
+vals = []
+config_ele = []
+head = config.sections()
+for key in head:
+    vals.append(config.options(key))
 
-user_ele = config.get("user_element", "user")
-user_next = config.get("user_element", "next")
-
-y_ele = config.get("y_element", "y")
-y_next = config.get("y_element", "next")
-
-id_ele = config.get("sheet_element", "user_id")
-mon_ele = config.get("sheet_element", "month")
-day_ele = config.get("sheet_element", "day")
-radio1_ele = config.get("sheet_element", "radio1")
-radio2_ele = config.get("sheet_element", "radio2")
-next1_ele = config.get("sheet_element", "next1")
-next2_ele = config.get("sheet_element", "next2")
-
+for key in range(len(head)):
+    for val in vals[key]:
+        config_ele.append(config.get(head[key], val))
+        
 today = date.today()
 
 option = webdriver.ChromeOptions()
@@ -31,23 +22,23 @@ option.add_argument("-incognito")
 option.add_experimental_option("excludeSwitches", ['enable-automation'])
 
 google = webdriver.Chrome(options=option)
-google.get(url)
+google.get(config_ele[0])
 
-google.find_element_by_name(user_ele).send_keys(user)
-google.find_element_by_xpath(user_next).click()
+google.find_element_by_name(config_ele[4]).send_keys(config_ele[1])
+google.find_element_by_xpath(config_ele[5]).click()
 
 google.implicitly_wait(5)
 
-google.find_element_by_name(y_ele).send_keys(x)
-google.find_element_by_xpath(y_next).click()
+google.find_element_by_name(config_ele[6]).send_keys(config_ele[2])
+google.find_element_by_xpath(config_ele[7]).click()
 
 google.implicitly_wait(50)
 
-google.find_element_by_class_name(id_ele).send_keys(id)  
-google.find_element_by_xpath(mon_ele).send_keys(today.month)
-google.find_element_by_xpath(day_ele).send_keys(today.day)
-google.find_element_by_xpath(radio1_ele).click()
-google.find_element_by_xpath(next1_ele).click()
-google.find_element_by_xpath(radio2_ele).click()
-google.find_element_by_xpath(next2_ele).click()
-# submit
+google.find_element_by_class_name(config_ele[8]).send_keys(config_ele[3])  
+google.find_element_by_xpath(config_ele[9]).send_keys(today.month)
+google.find_element_by_xpath(config_ele[10]).send_keys(today.day)
+google.find_element_by_xpath(config_ele[11]).click()
+google.find_element_by_xpath(config_ele[12]).click()
+google.find_element_by_xpath(config_ele[13]).click()
+google.find_element_by_xpath(config_ele[14]).click()
+google.find_element_by_xpath(config_ele[15]).click()
